be0e62397035083acd1ab439a8556f5bffa28df3
politelychaotic <94557891+politelychaotic@users.noreply.github.com>
2025-06-19 11:44:06 -0500

    Update crm.cpp
    Add main menu, edit_menu, and last_code classes

--- crm.cpp
+++ crm.cpp
@@ -43,5 +43,145 @@ private:
 
 class account
 {
+public:
+    void bill_list(void);
+    void prepare_bill(void);
+    int last_billno(void);
+    void add_bill(int, int t_itemcode, char*t_itemname, float t_qty, float t_cost, float t_price);
+private:
+    int code, billno, length;
+    int dd, mm, yy;
+    float cost, price, quantity;
+    char name[30];
+};
 
+
+//This function creates main menu and calls other funcs
+void menu::main_menu(void)
+{
+    clrscr();
+    char ch;
+    while(1)
+    {
+        clrscr();
+        gotoxy(10,3);
+        cout << "*******************************************************";
+        gotoxy(10,23);
+        cout << "*******************************************************";
+        gotoxy(28,6);
+        cout << " SALES MANAGEMENT";
+        gotoxy(28,7);
+        cout << "---------------------------";
+        gotoxy(30,9);
+        cout << "1: Purchase Products";
+        gotoxy(30,11);
+        cout << "2: List of Products";
+        gotoxy(30,13);
+        cout << "3: Edit Products File";
+        gotoxy(30,15);
+        cout << "4: Bills Report";
+        gotoxy(30,17);
+        cout << "0: Quit";
+        gotoxy(30,20);
+        cout << "Enter Option:";
+        ch = getche();
+        if(ch=='1') 
+        {
+            product p;
+            p.purchase();
+        }
+        else if(ch=='2')
+        {
+            product p;
+            p.list_of_items();
+        }
+        else if(ch=='3')
+        {
+            edit_menu();
+        }
+        else if(ch=='4')
+        {
+            account a;
+            a.bill_list();
+        }
+        else if(ch=='0')
+        {
+            break;
+        }
+    }
+}
+
+
+// EDIT MENU
+
+void menu::edit_menu(void)
+{
+    char ch;
+    while(1)
+    {
+        clrsrc();
+        gotoxy(10,3);
+        cout << "**************************************************";
+        gotoxy(10,23);
+        cout << "**************************************************";
+        gotoxy(32,6);
+        cout << "Edit Menu";
+        gotoxy(32,7);
+        cout << "---------------------";
+        gotoxy(32,10);
+        cout << "1: Add Products";
+        gotoxy(32,12);
+        cout << "2: Modify Products";
+        gotoxy(32,14);
+        cout << "3: Delete Products";
+        gotoxy(32,16);
+        cout << "0: Exit";
+        gotoxy(32,19);
+        cout << "Enter Option:";
+        ch=getche();
+        if(ch=='1')
+        {
+            product p;
+            p.add_item();
+            break;
+        }
+        else if(ch=='2')
+        {
+            product p;
+            p.modify_item();
+            break;
+        }
+        else if(ch=='3')
+        {
+            product p;
+            p.delete_item();
+            break;
+        }
+        else if(ch=='0')
+        {
+            break;
+        }
+    }
+}
+
+
+// Returns code of last record in the product file (product.dat)
+
+int product::last_code(void)
+{
+    fstream file;
+file.open("product.dat", ios::in);
+file.seekg(0,ios::beg);
+int t=0;
+while(file.read((char*)this,sizeof(product)))
+    t = itemcode;
+fileclose();
+return t;
+}
+
+// Displays the list of items
+
+void product::list_of_items(void)
+{
+    
 }
